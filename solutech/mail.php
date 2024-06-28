bac283cb35975d656beb8bc02ec57bbfb8428a8e
sistemasisosgruop <sistemasisosgroup@gmail.com>
2024-06-28 12:13:08 -0500

    cambio de correo 28062024 1213 mail.php

--- solutech/mail.php
+++ solutech/mail.php
@@ -25,7 +25,7 @@
         }
 
         // Set the recipient email address.
-        $recipient = "info@example.com";
+        $recipient = "sistemas@isosgroupsac.com";
 
         // Set the email sub.
         $sub = "Mail contact from $name";
